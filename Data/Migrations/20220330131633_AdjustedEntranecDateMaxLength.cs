47967a96b8c42ac858a048a8c3b2a6445353d03b
P-A-D <89439000+P-A-D@users.noreply.github.com>
2022-03-30 17:55:52 +0430

    Adjusted Student table data constraints. Run an update-database command to apply the changes to your db structure.

---
+++ Data/Migrations/20220330131633_AdjustedEntranecDateMaxLength.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EntV.Data.Migrations
+{
+    public partial class AdjustedEntranecDateMaxLength : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "EntranceDate",
+                table: "Students",
+                maxLength: 2,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "EntranceDate",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 2);
+        }
+    }
+}
